863b94847201b4f767d273214fa2812ce5db7771
Matthew Giarra <matthew.giarra@gmail.com>
2016-10-07 02:56:31 -0400

    Tried speeding up the gaussian fit function.

--- filtering/fit_gaussian_2D.m
+++ filtering/fit_gaussian_2D.m
@@ -5,15 +5,13 @@ function [AMPLITUDE, STD_DEV_Y, STD_DEV_X, YC, XC, OFFSET, ARRAY] = fit_gaussian
 % Size of the domain
 [region_height, region_width] = size(INPUT_SIGNAL);
 
-% Zero frequency pixels
-yc_domain = (region_height + 1) / 2 + 1/2 * (1 - mod(region_height, 2));
-xc_domain = (region_width  + 1) / 2 + 1/2 * (1 - mod(region_width,  2));
+xv2 = ((1 : region_width) - fourier_zero(region_width)).^2;
+yv2 = ((1 : region_height) - fourier_zero(region_height)).^2;
 
-% Domain as matrices
-[X, Y] = meshgrid(1 : region_width, 1 : region_height);
+[X2, Y2] = meshgrid(xv2, yv2);
 
 % Reshape the data into vectors
-xdata = [Y(:), X(:)];
+xdata = [Y2(:), X2(:)];
 
 if nargin < 2
     % Std devation guess
@@ -36,10 +34,8 @@ amplitude_guess = max(INPUT_SIGNAL(:));
 % Offset guess
 offset_guess = min(INPUT_SIGNAL(:));
 
-xc_guess = [yc_domain, xc_domain];
-
 % Guesses
-x0 = [amplitude_guess; std_dev_guess(:); xc_guess(:); offset_guess];
+x0 = [amplitude_guess; std_dev_guess(:); offset_guess];
 
 % Input function data
 ydata = INPUT_SIGNAL(:);
@@ -54,12 +50,10 @@ params = lsqcurvefit(@gauss_fun,x0,xdata,ydata, [], [], opts);
 AMPLITUDE = params(1);
 STD_DEV_Y = abs(params(2));
 STD_DEV_X = abs(params(3));
-YC = params(4);
-XC = params(5);
-OFFSET = params(6);
+OFFSET = params(4);
 
 % Caluclate the gaussian
-ARRAY = (AMPLITUDE * exp(-(X - XC).^2 / (2 * STD_DEV_X^2)) .* exp(-(Y - YC).^2 / (2 * STD_DEV_Y^2))) + OFFSET;
+ARRAY = (AMPLITUDE * exp(-X2 / (2 * STD_DEV_X^2)) .* exp(-Y2 / (2 * STD_DEV_Y^2))) + OFFSET;
 
 end
 
@@ -78,23 +72,19 @@ function F = gauss_fun(x, xdata)
     % Standard deviation in the columns direction
     sx = x(3);
     
-    % Center positions
-    yc = x(4);
-    xc = x(5);
-    
     % Zero offset (minimum value)
-    B = x(6);
+    B = x(4);
     
     % X is the coordinate data
-    X = xdata(:, 2);
+    X2 = xdata(:, 2);
     
     % Y is the data to be fit, 
     % i.e. the measured data
-    Y = xdata(:, 1);
+    Y2 = xdata(:, 1);
 
     % This is the Gaussian function
-	F = B + A * exp(- (Y- yc).^2 ./ (2 * sy^2)) .* ...
-           exp(-(X - xc).^2 ./ (2 * sx^2));
+	F = B + A * exp(- Y2 ./ (2 * sy^2)) .* ...
+           exp(-X2 ./ (2 * sx^2));
 		   
 		   
 		   
